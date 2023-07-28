e5987216a8efd941f927976ee46bac1834b0f9b5
sergio-1099 <sergioguerra6188@gmail.com>
2023-07-28 18:16:08 -0400

    Open and read the original 'cluster.csv' file

--- csv-creator.py
+++ csv-creator.py
@@ -5,3 +5,7 @@ header = ["Unit", "File Name Y/M/D", "File Time", "Initials", "Date", "Species",
 with open('new_cluster.csv', 'w') as new_file:
     csv_writer = csv.writer(new_file)
     csv_writer.writerow(header)
+
+    with open('cluster.csv', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        
