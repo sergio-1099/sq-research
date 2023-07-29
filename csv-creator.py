db83d52bef910b1a11226ce4f64c861d8826807f
sergio-1099 <sergioguerra6188@gmail.com>
2023-07-29 13:34:54 -0400

    Create a class for data entries to sort filenames

--- csv-creator.py
+++ csv-creator.py
@@ -1,5 +1,62 @@
 import csv
 
+class Data:
+    zero_min = ''
+    one_min = ''
+    two_min = ''
+    three_min = ''
+    four_min = ''
+
+    def __init__(self, filename, offset, species):
+        self.filename = filename
+        self.offset = offset
+        self.species = species
+
+    def set_offset(self, new_offset):
+        self.offset = new_offset
+
+    def label_offset(self):
+        self.offset = int(self.offset / 60)
+        if self.offset == 0:
+            self.zero_min = 'X'
+        elif self.offset == 1:
+            self.one_min = 'X'
+        elif self.offset == 2:
+            self.two_min = 'X'
+        elif self.offset == 3:
+            self.three_min = 'X'
+        elif self.offset == 4:
+            self.four_min = 'X'
+
+    def create_data_row(self):
+        new_row_array = []
+
+        first_columns = self.filename.split("-")
+
+        new_row_array.append(first_columns[0])
+        new_row_array.append(first_columns[1])
+        new_row_array.append(first_columns[2])
+
+        new_row_array[2] = new_row_array[2][:-4]
+
+        new_row_array.append('')
+        new_row_array.append('')
+        new_row_array.append(self.species)
+        new_row_array.append('')
+        new_row_array.append(self.zero_min)
+        new_row_array.append(self.one_min)
+        new_row_array.append(self.two_min)
+        new_row_array.append(self.three_min)
+        new_row_array.append(self.four_min)
+        new_row_array.append('')
+        new_row_array.append('')
+        new_row_array.append('')
+        new_row_array.append('')
+        new_row_array.append('')
+
+        return new_row_array
+
+
 header = ["Full name", "Unit", "File Name Y/M/D", "File Time", "Initials", "Date", "Species", "No. Ind.", "Second/Offset Detected", "TMTC", "VT", "TBC", "Comment", "Verified"]
 
 final_header = ["Unit", "File Name Y/M/D", "File Time", "Initials", "Date", "Species", "No. Ind.", "0min", "1min", "2min", "3min", "4min", "TMTC", "VT", "TBC", "Comment", "Verified"]
